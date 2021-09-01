0c0c3ea455bf69ed4acdc81042310c95f383efab
Andre Barbosa <andre.jbarbosa@pm.me>
2021-08-31 20:46:28 -0400

    Update.

--- Chapter_2/dweight.c
+++ Chapter_2/dweight.c
@@ -7,16 +7,17 @@
 
 int main(int argc, char *argv[])
 {
-    int length = atoi(argv[1]);
-    int width  = atoi(argv[2]);
-    int height = atoi(argv[3]);
-    int volume = length * width * height;
-    int weight = (volume + 165) / 166;
+    float length  = atof(argv[1]);
+    float width   = atof(argv[2]);
+    float height  = atof(argv[3]);
+    float volume  = length * width * height;
+    float divisor = atof(argv[4]);
+    float weight  = volume / divisor;
 
-    if (argc == 4) {
-        printf("\nDimensions: %dx%dx%d\n", length, width, height);
-        printf("Volume (cubic inches): %d\n", volume);
-        printf("Dimensional weight (pounds): %d\n", weight);
+    if (argc == 5) {
+        printf("\nL x W x H: %.1f x %.1f x %.1f in.\n", length, width, height);
+        printf("Volume: %.1f cu in.\n", volume);
+        printf("Dimensional weight: %.0lf lb.\n", weight);
         return 0;
     } else
         return 1;

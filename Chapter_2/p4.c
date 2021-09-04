5fd1ef788bb538e65afc2a7c73ac04dd80f3eedf
Andre Barbosa <andre.jbarbosa@pm.me>
2021-09-03 20:45:51 -0400

    Update.

---
+++ Chapter_2/p4.c
@@ -0,0 +1,24 @@
+/*
+ * This program declares several int and float variables and prints them to
+ * stdout without initializing them.
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    int   a, b, c;
+    float e, f, g;
+
+    printf("Integers:\n");
+    printf("Value of a: %d\n", a);
+    printf("Value of b: %d\n", b);
+    printf("Value of c: %d\n", c);
+
+    printf("\nFloats:\n");
+    printf("Value of e: %f\n", e);
+    printf("Value of f: %f\n", f);
+    printf("Value of g: %f\n", g);
+
+    return 0;
+}

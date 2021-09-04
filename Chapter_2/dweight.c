e841e443410c641074c8060888f60fb18f08dfe0
Andre Barbosa <andre.jbarbosa@pm.me>
2021-09-03 20:29:13 -0400

    Update.

--- Chapter_2/dweight.c
+++ Chapter_2/dweight.c
@@ -12,12 +12,11 @@ int main(int argc, char *argv[])
     float height  = atof(argv[3]);
     float volume  = length * width * height;
     float divisor = atof(argv[4]);
-    float weight  = volume / divisor;
 
     if (argc == 5) {
         printf("\nL x W x H: %.1f x %.1f x %.1f in.\n", length, width, height);
         printf("Volume: %.1f cu in.\n", volume);
-        printf("Dimensional weight: %.0lf lb.\n", weight);
+        printf("Dimensional weight: %.0lf lb.\n", volume / divisor);
         return 0;
     } else
         return 1;
---
+++ Chapter_2/hello.c
@@ -0,0 +1,12 @@
+/*
+ * Kernighan and Ritchie's famous "hello, world" program.
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("hello, world\n");
+
+    return 0;
+}

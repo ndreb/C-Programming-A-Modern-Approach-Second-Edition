5941833e992232eff45efb0b6a9d8a2c766a4e52
Andre Barbosa <andre.jbarbosa@pm.me>
2021-09-04 19:18:19 -0400

    Update.

---
+++ Chapter_2/pp1.c
@@ -0,0 +1,21 @@
+/*
+ * Chapter 2
+ * Programming Project 1
+ * This program prints a check mark using asterisks.
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("\n"
+           "        *\n"
+           "       *\n"
+           "      *\n"
+           " *   *\n"
+           "  * *\n"
+           "   *\n"
+           );
+
+    return 0;
+}

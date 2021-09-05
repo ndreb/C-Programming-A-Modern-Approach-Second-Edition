947428c235fda7034d4eee054986b7963d02be9c
Andre Barbosa <andre.jbarbosa@pm.me>
2021-09-04 20:08:32 -0400

    Update.

---
+++ Chapter_2/vsphere.c
@@ -0,0 +1,22 @@
+/*
+ * This program computes the volume of a sphere, using the formula:
+ * V = (4 / 3) * (pi * r^3)
+ */
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define PI 3.14159265358979323846L
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+        return 1;
+    else {
+        float radius = atof(argv[1]);
+        float volume = (4.0f / 3.0f) * (PI * pow(radius, 3));
+        printf("%.4f", volume);
+        return 0;
+    }
+}

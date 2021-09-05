ad616f1cc98c0033731d384009e30aa2407c5af2
Andre Barbosa <andre.jbarbosa@pm.me>
2021-09-05 18:50:36 -0400

    Update.

---
+++ Chapter_2/pp5.c
@@ -0,0 +1,22 @@
+/*
+ * Computes f(x) = 3x^5 + 2x^4 - 5x^3 - x^2 + 7x - 6
+ */
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+        return 1;
+    else {
+        double x = atof(argv[1]);
+        double y = (3 * (pow(x, 5))) + (2 * (pow(x, 4))) - (5 * (pow(x, 3))) \
+                   - (pow(x, 2)) + (7 * x) - 6;
+
+        printf("%f", y);
+
+        return 0;
+    }
+}

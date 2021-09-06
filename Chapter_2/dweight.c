f2c4561ba6bf889c52b7116f2893fb678514b209
Andre Barbosa <andre.jbarbosa@pm.me>
2021-09-05 20:49:42 -0400

    Update.

--- Chapter_2/dweight.c
+++ Chapter_2/dweight.c
@@ -12,8 +12,8 @@ int main(int argc, char *argv[])
     float length  = atof(argv[1]);
     float width   = atof(argv[2]);
     float height  = atof(argv[3]);
-    float volume  = length * width * height;
     float divisor = atof(argv[4]);
+    float volume  = length * width * height;
 
     if (argc == 5) {
         printf("\nL x W x H: %.1f x %.1f x %.1f in.\n", length, width, height);

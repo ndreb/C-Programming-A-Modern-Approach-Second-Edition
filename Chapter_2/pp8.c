a967838ec79341c3e84180eb6e1914329013ef77
Andre Barbosa <andre.jbarbosa@pm.me>
2021-09-05 23:28:23 -0400

    Update.

--- Chapter_2/pp8.c
+++ Chapter_2/pp8.c
@@ -2,10 +2,27 @@
  * Chapter 2
  * Programming Project 8
  *
- * Computes the remaining balance on a loan after the first, second, and third
- * monthly payments.
+ * Computes the remaining balance on a loan after n monthly payments.
  */
 
+#include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void)
+int main(int argc, char *argv[])
+{
+    if (argc != 5)
+        return 1;
+    else {
+        double P = atof(argv[1]);        /* Principle */
+        double r = atof(argv[2]) / 1200; /* monthly interest rate */
+        double m = atof(argv[3]);        /* monthly payment */
+        double n = atof(argv[4]);        /* number of months */
+        /* Balance after n months */
+        double B = pow((1 + r), n) * P - (pow((1 + r), n) - 1) / r * m;
+
+        printf("Balance remaining after %.0f payment(s): $%.2f\n", n, B);
+
+        return 0;
+    }
+}

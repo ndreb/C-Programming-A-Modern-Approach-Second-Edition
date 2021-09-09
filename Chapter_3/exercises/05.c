c2aa27ef1d4f878654b5bb5417fbad672e2eb614
Andre Barbosa <andre.jbarbosa@pm.me>
2021-09-09 18:02:00 -0400

    Update.

---
+++ Chapter_3/exercises/05.c
@@ -0,0 +1,17 @@
+/*
+ * Chapter 3
+ * Exercise 5
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    int i;
+    float x, y;
+
+    scanf("%f%d%f", &x, &i, &y);
+    printf("%f, %d, %f\n", x, i, y);
+
+    return 0;
+}

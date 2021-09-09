5d9ab500c2ce48c1b737dbf1e072424287dce41d
Andre Barbosa <andre.jbarbosa@pm.me>
2021-09-09 16:29:34 -0400

    Update.

--- Chapter_3/exercises/01.c
+++ Chapter_3/exercises/01.c
@@ -11,4 +11,6 @@ int main(void)
     printf("%12.5e\n", 30.253);
     printf("%.4f\n", 83.162);
     printf("%6.2g\n", .0000009979);
+
+    return 0;
 }
---
+++ Chapter_3/exercises/02.c
@@ -0,0 +1,18 @@
+/*
+ * Chapter 3
+ * Exercise 2
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    float x;
+
+    printf("%-8.1e\n", x);
+    printf("%10.6e\n", x);
+    printf("%-8.3f\n", x);
+    printf("%6.0f\n", x);
+
+    return 0;
+}

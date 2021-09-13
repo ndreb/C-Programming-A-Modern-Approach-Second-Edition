9cb2296309a675d9ac66a87fb396e411979caee5
Andre Barbosa <andre.jbarbosa@pm.me>
2021-09-13 18:37:03 -0400

    Update.

---
+++ Chapter_3/projects/03.c
@@ -0,0 +1,24 @@
+/*
+ * Chapter 3
+ * Programming Project 3
+ *
+ * Breaks down an ISBN number.
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    int prefix, group_id, publisher_code, item_number, check_digit;
+
+    printf("Enter ISBN: ");
+    scanf("%d-%d-%d-%d-%d", &prefix, &group_id, &publisher_code, &item_number,\
+            &check_digit);
+    printf("GS1 prefix: %d\n", prefix);
+    printf("Group indentifier: %d\n", group_id);
+    printf("Publisher code: %d\n", publisher_code);
+    printf("Item number: %d\n", item_number);
+    printf("Check digit: %d\n", check_digit);
+
+    return 0;
+}

d066500dc014627bf9db42782c8d654d6d31701c
Andre Barbosa <andre.jbarbosa@pm.me>
2021-09-21 16:36:31 -0400

    Update.

---
+++ chapter_4/exercises/02.c
@@ -0,0 +1,18 @@
+/*
+ * Chapter 4
+ * Exercise 2
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    int i = 9;
+    int j = 7;
+    int w = (-i) / j;
+    int y = -(i / j);
+
+    printf("%d, %d", w, y);
+
+    return 0;
+}

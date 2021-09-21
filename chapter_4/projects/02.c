7136df3c92e15dd41a0e602405f5727e7e23f6fe
Andre Barbosa <andre.jbarbosa@pm.me>
2021-09-21 17:11:16 -0400

    Update.

---
+++ chapter_4/projects/02.c
@@ -0,0 +1,21 @@
+/*
+ * Chapter 4
+ * Programming Project 1
+ *
+ * Takes a three-digit number as input, reverses the digits, then prints the
+ * result.
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+
+    printf("Enter a three-digit number: ");
+    scanf("%d", &n);
+
+    printf("The reversal is: %d%d%d", n % 10, n / 10 % 10, n / 100);
+
+    return 0;
+}

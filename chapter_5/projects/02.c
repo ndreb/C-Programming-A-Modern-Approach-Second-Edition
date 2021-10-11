7020ed918225c081835826f13618a78f328c2555
Andre Barbosa <andre.jbarbosa@pm.me>
2021-10-11 18:47:16 -0400

    Update.

--- chapter_5/projects/02.c
+++ chapter_5/projects/02.c
@@ -3,23 +3,29 @@
  */
 
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
     int hour, minute;
+    char am[3] = "AM";
+    char pm[3] = "PM";
+    char suffix[3];
 
     printf("Enter a 24-hour time: ");
     scanf("%d:%d", &hour, &minute);
 
     if (hour == 0) {
         hour = 12;
-        printf("Equivalent 12-hour time: %d:%.2d AM\n", hour, minute);
+        strcpy(suffix, am);
     } else if (hour > 12) {
         hour -= 12;
-        printf("Equivalent 12-hour time: %d:%.2d PM\n", hour, minute);
+        strcpy(suffix, pm);
     } else {
-        printf("Equivalent 12-hour time: %d:%.2d AM\n", hour, minute);
+        strcpy(suffix, am);
     }
+
+    printf("Equivalent 12-hour time: %d:%.2d %s\n", hour, minute, suffix);
 
     return 0;
 }
